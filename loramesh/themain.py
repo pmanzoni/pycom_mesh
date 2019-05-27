f137799dbd107eb05ebd0e0dbe5bea7b51938417
pmanzoni <manzoni@ieee.org>
2019-05-27 19:50:15 +0200

    Fixed bugs

--- loramesh/themain.py
+++ loramesh/themain.py
@@ -19,29 +19,32 @@ import machine
 
 from lorameshlib import Loramesh
 
+UDPPORT = 1234
 PYMESHSTATE = ["PYMESH_ROLE_DISABLED", "PYMESH_ROLE_DETACHED", "PYMESH_ROLE_CHILD", "PYMESH_ROLE_ROUTER", "PYMESH_ROLE_LEADER"]
 
-# handler responsible for receiving packets on UDP Pymesh socket
-def receive_pack():
-    print("ENTERING UDP HANDLER")
-    # listen for incomming packets
+# Handler responsible for receiving packets on UDP Pymesh socket
+def receive_pack(s):
+    global device_lora_mac
+
     while True:
         rcv_data, rcv_addr = s.recvfrom(128)
-        if len(rcv_data) == 0:
-            break
-        rcv_ip = rcv_addr[0]
-        rcv_port = rcv_addr[1]
-        print('Incomming %d bytes from %s (port %d)'%(len(rcv_data), rcv_ip, rcv_port))
-        print(rcv_data)
-        # could send some ACK pack:
-        if rcv_data.startswith("Hello"):
-            try:
-                s.sendto('ACK ' + MAC + ' ' + str(rcv_data)[2:-1], (rcv_ip, rcv_port))
-            except Exception:
-                pass
-        mesh.blink(7, .3)
+        if len(rcv_data) > 0:
+            rcv_ip   = rcv_addr[0]
+            rcv_port = rcv_addr[1]
+            print('received %d bytes from %s (port %d)' % (len(rcv_data), rcv_ip, rcv_port))
+            print("\t> %s" % rcv_data)
+
+            # Sends an ACK packet if it was an "Hello" packet
+            if rcv_data.startswith("Hello"):
+                try:
+                    s.sendto('ACK ' + device_lora_mac + ' ' + str(rcv_data)[2:-1], (rcv_ip, rcv_port))
+                except Exception as e:
+                    print("inside 'receive_pack()': %s" % str(e))
+                    pass
+            mesh.blink(7, .3)
 
 
+########################################
 
 pycom.wifi_on_boot(False)
 pycom.heartbeat(False)
@@ -55,65 +58,74 @@ while not lora_active:
     except Exception as e:
         print("DISASTER! exception opening Lora: "+str(e))
         time.sleep(2)
-MAC = str(ubinascii.hexlify(lora.mac()))[2:-1]
-print("LoRa MAC: %s"%MAC)
+device_lora_mac = str(ubinascii.hexlify(lora.mac()))[2:-1]
+print("LoRa MAC: %s"%device_lora_mac)
 
 mesh = Loramesh(lora)
-
 
 # waiting until it is connected to Mesh network
 cstate = mesh._state_update()
 while (cstate<2):       # exits when either "PYMESH_ROLE_CHILD", "PYMESH_ROLE_ROUTER" or "PYMESH_ROLE_LEADER"
     mesh.led_state()
-    print("%d: looping... [%s]"%(time.time(), PYMESHSTATE[cstate]))
+    print("%d: waiting... [%s]"%(time.time(), PYMESHSTATE[cstate]))
     time.sleep(2)
     cstate = mesh._state_update()
 
+print('Current status: %s'  % PYMESHSTATE[cstate])
 
-print('Neighbors found: %s'%mesh.neighbors())
+node_ip_addr = mesh.ip()
 
 # create UDP socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-myport = 1234
-s.bind(myport)
-
-pack_num = 1
-msg = "Hello World! MAC: " + MAC + ", pack: "
-ip = mesh.ip()
+s.bind(UDPPORT)
 mesh.mesh.rx_cb(receive_pack, s)
 
-# infinite main loop
+
+# Main loop
+pack_num = 0
 while True:
     mesh.led_state()
-    print("%d: state: %s, is singleton: %s, IP: %s"%(time.time(), mesh.cli('state'), mesh.cli('singleton'), mesh.ip()))
+    c_state = mesh.cli('state')
+    if "true" in mesh.cli('singleton'):
+        single = "is singleton"
+    else:
+        single = "not singleton"
+    print("%d: state: %s, IP: %s [%s]" % (time.time(), c_state, node_ip_addr, single))
 
     # check if topology changes, maybe RLOC IPv6 changed
-    new_ip = mesh.ip()
-    if ip != new_ip:
-        print("IP changed from: %s to %s"%(ip, new_ip))
-        ip = new_ip
+    new_node_ip_addr = mesh.ip()
+    if node_ip_addr != new_node_ip_addr:
+        print("IP changed from: %s to %s"%(node_ip_addr, new_node_ip_addr))
+        node_ip_addr = new_node_ip_addr
 
     # update neighbors list
     neigbors = mesh.neighbors_ip()
-    print("%d neighbors, IPv6 list: %s"%(len(neigbors), neigbors))
+    print("%d: %d neighbors, IPv6 list: %s" % (time.time(),len(neigbors), neigbors))
 
     # send PING and UDP packets to all neighbors
     for neighbor in neigbors:
-        if mesh.ping(neighbor) > 0:
-            print('Ping OK from neighbor %s'%neighbor)
-            mesh.blink(10, .1)
-        else:
-            print('Ping not received from neighbor %s'%neighbor)
 
-        time.sleep(10)
+        try:
+            tping = mesh.ping(neighbor)
+            if tping > 0:
+                print('Ping OK from neighbor %s'%neighbor)
+                mesh.blink(10, .1)
+            else:
+                print('Ping not received from neighbor %s'%neighbor)
+        except Exception as e:
+            print("exception doing PING: "+str(e))
+            pass
+
+        time.sleep(5)
 
         pack_num = pack_num + 1
         try:
-            s.sendto(msg + str(pack_num), (neighbor, myport))
-            print('Sent message to %s'%(neighbor))
-        except Exception:
+            s.sendto("Hello World! from node: " + device_lora_mac + ", pack: " + str(pack_num), (neighbor, UDPPORT))
+            print('Sent message to %s' % (neighbor))
+        except Exception as e:
+            print("exception sending packet: "+str(e))
             pass
-        time.sleep(20 + machine.rng()%20)
+        time.sleep(5 + machine.rng()%10)
 
     # random sleep time
-    time.sleep(30 + machine.rng()%30)
+    time.sleep(5 + machine.rng()%10)
